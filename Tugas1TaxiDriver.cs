f451bcca885a11e5f5fc16affe46fdf2f06598cc
Tania Damayanti <127859384+taniadmynt0401@users.noreply.github.com>
2023-04-05 00:34:12 +0700

    Add files via upload

---
+++ Tugas1TaxiDriver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasTaxiDriver
+{
+    public class TugasTaxiDriver
+    {
+        public string DriverName { get; set; }
+        public Boolean OnDuty { get; set; }
+        public string NumPassanger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            Console.WriteLine("On Duty : {0}", OnDuty);
+            Console.WriteLine("Num Passanger : {0}", NumPassanger);
+
+        }
+        public void PickUpPassanger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang\n", DriverName);
+
+        }
+
+        public void DropOffPassanger()
+
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang\n", DriverName);
+        }
+    }
+}
