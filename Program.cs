78650eafde261f49a52c8969139916feb02f3008
Tania Damayanti <127859384+taniadmynt0401@users.noreply.github.com>
2023-04-05 00:30:47 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasTaxiDriver
+{
+    public class program
+    {
+        static void Main(string[] ags)
+        {
+            TugasTaxiDriver taxi = new TugasTaxiDriver();
+
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassanger = "10";
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassanger();
+            taxi.DropOffPassanger();
+
+            Console.ReadKey();
+        }
+    }
+}
